e9356a0cdde43fc68344bb9698489f0b9181e143
Sylvain DNS <sylvain.denyse@gmail.com>
2019-05-26 12:28:06 +0200

    implement message broadcasting on chat socket

--- src/socket/chatSocket.js
+++ src/socket/chatSocket.js
@@ -1,7 +1,11 @@
 export const chatSocket = socket => {
   const chat = socket.of('/chat')
 
-  chat.on('connection', () => {
+  chat.on('connection', clientSocket => {
     console.log('Client connected on /chat')
+
+    clientSocket.on('send', message => {
+      clientSocket.broadcast.emit('receive', message)
+    })
   })
 }
