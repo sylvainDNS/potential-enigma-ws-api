49111d5a379024551bde34104e8b6d09793ce212
Sylvain DNS <sylvain.denyse@gmail.com>
2019-05-28 00:41:41 +0200

    add test data

---
+++ docker/postgres/sql/03-insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.game (game_id, name, "createdAt", "deletedAt", "completedAt") VALUES ('657356fd-f090-4fcd-99df-bd2c7c8b5794', 'Vincent vs Maxime', '2019-05-27 22:35:53.459183', null, null);
+INSERT INTO public.game (game_id, name, "createdAt", "deletedAt", "completedAt") VALUES ('fb54b7f7-7059-452d-9c36-6fbff38d870f', 'Matthieu vs Florence', '2019-05-27 22:36:03.964843', null, null);
+INSERT INTO public.game (game_id, name, "createdAt", "deletedAt", "completedAt") VALUES ('4c705682-7568-4f91-833b-c700aa7904e9', 'David vs Guillaume', '2019-05-27 22:36:20.560078', null, null);
+INSERT INTO public.game (game_id, name, "createdAt", "deletedAt", "completedAt") VALUES ('4aaa96c2-6831-4243-8d28-5d6c93c797c4', 'Enzo vs Arnaud', '2019-05-27 22:36:11.852093', null, '2019-05-27 22:36:22.943080');
